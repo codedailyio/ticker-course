b374ffec38b601ae3f8b3dbec54b91b19ff7e56b
Jason Brown <browniefed@gmail.com>
2018-02-03 15:03:26 -0800

    Add hidden End

--- App.js
+++ App.js
@@ -5,13 +5,50 @@ const numberRange = Array(10)
   .fill()
   .map((x, i) => i);
 
+const getPosition = (value, height) => parseInt(value, 10) * height * -1;
+const getTranslateStyle = position => ({
+  transform: [
+    {
+      translateY: position,
+    },
+  ],
+});
+
 export default class App extends Component {
+  state = {
+    measured: false,
+    height: 0,
+  };
+
+  handleLayout = e => {
+    this.setState({
+      measured: true,
+      height: e.nativeEvent.layout.height,
+    });
+  };
   render() {
+    const { height, measured } = this.state;
+    const wrapStyle = measured ? { height } : styles.measure;
+
+    const transformStyle = getTranslateStyle(getPosition(0, height));
     return (
       <View style={styles.container}>
-      
+        <View style={[styles.hidden, wrapStyle]}>
+          <View style={transformStyle}>
+            {numberRange.map(v => {
+              return (
+                <Text key={v} style={styles.text}>
+                  {v}
+                </Text>
+              );
+            })}
+          </View>
+        </View>
+        <Text style={[styles.text, styles.measure]} onLayout={this.handleLayout}>
+          0
+        </Text>
       </View>
-    )
+    );
   }
 }
 
@@ -21,5 +58,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  measure: {
+    opacity: 0,
+  },
+  hidden: {
+    overflow: "hidden",
+  },
+  text: {
+    fontSize: 80,
+    color: "#333",
   },
 });
